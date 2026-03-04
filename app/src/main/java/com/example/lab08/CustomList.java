bb5257c2eff639e3f455aab239552da5b943f024
Kevin Cao <kcao36@outlook.comgit config --global user.email kcao36@outlook.comclear>
2026-03-03 17:49:57 -0700

    Write delete(), test passed

--- app/src/main/java/com/example/lab08/CustomList.java
+++ app/src/main/java/com/example/lab08/CustomList.java
@@ -18,4 +18,8 @@ public class CustomList {
     public boolean hasCity(City city) {
         return cities.contains(city);
     }
+
+    public void delete(City city){
+        cities.remove(city);
+    }
 }
