4361d929e5b5523e22ce888b06591852a4dbbe17
Kevin Cao <kcao36@outlook.comgit config --global user.email kcao36@outlook.comclear>
2026-03-03 17:42:05 -0700

    write delete test

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -2,6 +2,24 @@ package com.example.lab08;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+
 public class CustomListTest {
 
+    @Test
+    public void testHasCity() {
+        CustomList list = new CustomList();
+        City calgary = new City("Calgary", "AB");
+        list.addCity(calgary);
+        assertTrue(list.hasCity(calgary));
+    }
+
+    @Test
+    public void testDeleteCity() {
+        CustomList cityList = new CustomList();
+        City city = new City("Calgary", "AB");
+        cityList.addCity(city);
+
+        cityList.delete(city);
+        assertFalse(cityList.hasCity(city));
+    }
 }
